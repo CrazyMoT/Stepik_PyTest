f1647f2c9e5c2c1e87ed392310439eb959d1c195
Matvei Golikov <112405802+CrazyMoT@users.noreply.github.com>
2023-02-01 02:37:10 +0530

    Creating Files
    Creating Files

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+def test_add_to_cart_button_exists(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert add_to_cart_button, "Add to cart button is not found"
